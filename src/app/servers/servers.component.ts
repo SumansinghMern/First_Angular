e16a5a1b8009808ff52a8c4451ba45fee1f0eca7
Suman Singh <sumansingh7870@outlook.com>
2023-06-28 16:07:07 +0530

    tow way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,9 +8,31 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   allowServerAdd: boolean = false;
 
+  serverCreationStatus: string = "No Server Created!"
+
+
+  serverName = 'Test Server';
+
+  userName:string = ''
+
+  
+
   constructor(){
     setTimeout(() => {
       this.allowServerAdd  =true
-    },2000)
+    },5000)
+  }
+
+  onChangeServerName(event:Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+
+  }
+
+  onClearUserName(){
+    this.userName = '';
+  }
+
+  onChangeServerCreation(){
+    this.serverCreationStatus = 'Server Created and Server Name is ' + this.serverName
   }
 }
